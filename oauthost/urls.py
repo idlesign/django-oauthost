2d9436edcd9df70d614ba4c9f9bf2f942d2d8e74
idle sign <idlesign@yandex.ru>
2011-11-07 22:06:41 +0700

    urls.py now use named urls.

--- oauthost/urls.py
+++ oauthost/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('oauthost',
-    url(r'^auth/$', 'auth_views.endpoint_authorize'),
+    url(r'^auth/$', 'auth_views.endpoint_authorize', name='oauthost_authorize'),
     # SPEC: The [token] endpoint URI MUST NOT include a fragment component.
-    url(r'^token/$', 'auth_views.endpoint_token'),
+    url(r'^token/$', 'auth_views.endpoint_token', name='oauthost_token'),
 )
