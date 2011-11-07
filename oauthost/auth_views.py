ab673a2c0212e562325a665dd8fc434eb38a1779
idle sign <idlesign@yandex.ru>
2011-11-07 21:16:06 +0700

    Tokens type registry dictionary is moved from models to views.

--- oauthost/auth_views.py
+++ oauthost/auth_views.py
@@ -21,6 +21,11 @@ LOGGER = logging.getLogger('django.oauthost')
 REGISTRY_EP_AUTH_RESPONSE_TYPE = ['code', 'token']
 REGISTRY_EP_TOKEN_GRANT_TYPE = ['authorization_code', 'password', 'client_credentials', 'refresh_token']
 
+TOKEN_TYPE_BEARER = 'bearer'
+REGISTRY_TOKEN_TYPE = {
+    (TOKEN_TYPE_BEARER, _('Bearer')),
+}
+
 OAUTHOST_TEMPLATE_AUTHORIZE = 'oauthost/authorize.html'
 OAUTHOST_TEMPLATE_AUTHORIZE_ERROR = 'oauthost/authorize_error.html'
 OAUTHOST_TEMPLATE_AUTHORIZE_PROCEED = 'oauthost/authorize_proceed.html'
