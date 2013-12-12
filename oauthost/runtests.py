594fca8e210a93d4732e838097593c9d80f53364
idle sign <idlesign@yandex.ru>
2013-12-12 22:57:53 +0700

    Django 1.6 test runner rules adopted.

--- oauthost/runtests.py
+++ oauthost/runtests.py
@@ -1,14 +1,29 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+import sys
+import os
+
 from django.conf import settings
-from django.core.management import call_command
 
 
-if not settings.configured:
-    settings.configure(
-        INSTALLED_APPS=('django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'oauthost'),
-        DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
-        ROOT_URLCONF='oauthost.urls'
-    )
+APP_NAME = 'oauthost'
+
+
+def main():
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+    if not settings.configured:
+        settings.configure(
+            INSTALLED_APPS=('django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', APP_NAME),
+            DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
+            ROOT_URLCONF='oauthost.urls'
+        )
+
+    from django.test.utils import get_runner
+    runner = get_runner(settings)()
+    failures = runner.run_tests((APP_NAME,))
+
+    sys.exit(failures)
+
 
 if __name__ == '__main__':
-    call_command('test', 'oauthost')
+    main()
