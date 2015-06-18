442da8c00140ee79e2956b1048f08009924454c3
idle sign <idlesign@yandex.ru>
2015-06-18 20:22:34 +0600

    setup.py is updated.

--- setup.py
+++ setup.py
@@ -9,14 +9,19 @@ f.close()
 setup(
     name='django-oauthost',
     version='.'.join(map(str, VERSION)),
+    url='http://github.com/idlesign/django-oauthost',
+
     description='Reusable application for Django to protect your apps with OAuth 2.0.',
     long_description=readme,
+    license='BSD 3-Clause License',
+
     author='Igor `idle sign` Starikov',
     author_email='idlesign@yandex.ru',
-    url='http://github.com/idlesign/django-oauthost',
+
     packages=['oauthost'],
     include_package_data=True,
     zip_safe=False,
+
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -29,5 +34,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ]
 )
