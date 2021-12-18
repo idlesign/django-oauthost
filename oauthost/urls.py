e13d955c24f0b772933a0427ab3b1ead1056b906
idle sign <idlesign@yandex.ru>
2021-12-18 12:22:04 +0700

    Import fixes.

--- oauthost/urls.py
+++ oauthost/urls.py
@@ -1,19 +1,19 @@
 from django import VERSION
 
 try:
-    from django.conf.urls import re_path as url
+    from django.urls import re_path
 
 except ImportError:
     # Deprecated in new versions
-    from django.conf.urls import url
+    from django.conf.urls import url as re_path
 
 from .views import endpoint_authorize, endpoint_token
 
 urlpatterns_list = [
-    url(r'^auth/$', endpoint_authorize, name='oauthost_authorize'),
+    re_path(r'^auth/$', endpoint_authorize, name='oauthost_authorize'),
 
     # SPEC: The [token] endpoint URI MUST NOT include a fragment component.
-    url(r'^token/$', endpoint_token, name='oauthost_token'),
+    re_path(r'^token/$', endpoint_token, name='oauthost_token'),
 ]
 
 
